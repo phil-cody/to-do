b67163cdaf550b4d8427462b993491258a7afe89
Dmitry <maiiamphiphi@gmail.com>
2026-07-07 14:28:04 +0700

    fix function checkTheAvailabilityOfTheProjectAtTheTask

--- src/modules/utils/checkTheAvailabilityOfTheProjectAtTheTask.js
+++ src/modules/utils/checkTheAvailabilityOfTheProjectAtTheTask.js
@@ -8,13 +8,11 @@ import { getProjectById } from "@/modules/services/projectService";
 export function checkTheAvailabilityOfTheProjectAtTheTask() {
   state.projects.forEach((project) => {
     project.todoList.forEach((todo) => {
+      console.log(todo)
       if (todo.task_project === project.project_id) {        
         return;
       } else if (!todo.task_project || todo.task_project !== project.project_id) {
-        const currentProject = getProjectById(project.project_id);
-        const todoIndex = findTaskIndexByID(todo.task_id);
-        
-        currentProject.todoList[todoIndex].updateTodoProject(project.project_id);
+        todo.updateTodoProject(project.project_id);
       }
     });
   });
